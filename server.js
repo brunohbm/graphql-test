a0fa8ef096d6eb631ba4f5662c3e4f00109226dc
Bruno Henrique <brunohbm23@gmail.com>
2022-08-19 21:52:10 -0300

    Adding the schema of the shopping cart

--- server.js
+++ server.js
@@ -2,22 +2,73 @@ const express = require("express");
 const { buildSchema } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
 
+const app = express();
+
 const schema = buildSchema(`
     type Query {
-        price: Float
-        description: String
+        orders: [Order]
+        products: [Product]
+    }
+
+    type Product {
+        id: ID!
+        price: Float!
+        description: String!
+        reviews: [Review]
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
     }
 `);
 
-const app = express();
+const rootValue = {
+    products: [
+        {
+            id:' redshoe',
+            decription: 'Red Shoe',
+            price: 42.12,
+        },
+        {
+            id:'bluiejean',
+            description: 'Blue Jeans',
+            price: 55.55
+        }
+    ],
+    orders: [
+        {
+            date: '2005-05-05',
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id:' redshoe',
+                        decription: 'Red Shoe',
+                        price: 42.12,
+                    },
+                    quantity: 2,
+                }
+            ]
+        },
+    ],
+};
 
 app.use('/graphql', graphqlHTTP({
     schema,
+    rootValue,
     graphiql: true,
-    rootValue: {
-        price: 300,
-        description: 'Teste',
-    }
 }))
 
 app.listen(3000, () => {
